1bcb3ad8555e3e7859cfecea47512799b6067c68
sumit agrawal <sumit4iit@gmail.com>
2014-11-27 00:08:06 +0530

    Guessed the key length

--- set_1/1-6.py
+++ set_1/1-6.py
@@ -6,6 +6,20 @@ def hamming_distance(s1, s2):
     [call ''.encode('hex') before passing it as an argument]
     Computing number of bits that two strings differ in should do the trick.
     """
+    # Make strings of even size.
+    # Should not have any other side effect.
+    if len(s1)%2 != 0:
+        s1 = s1+'0'
+    if len(s2)%2 != 0:
+        s2 = s2+'0'
+
+    # Make strings of equal length.
+    # For the sake of sanity.
+    if len(s2)<len(s1):
+        s2 = s2+(len(s1) - len(s2))*'0'
+    if len(s2)<len(s1):
+        s1 = s1+(len(s2) - len(s1))*'0'
+
 
     cnt = 0
     res = int(s1,16) ^ int(s2,16)
@@ -18,8 +32,37 @@ def hamming_distance(s1, s2):
     return cnt
 
 
+def key_length(txt):
+    # Single iteratino works for now.
+    # TODO:// Add extra iterations and take avarage.
+    dkt = {}
+    for length in range(2,40):
+        dkt[length] = (hamming_distance(txt[:length], txt[length:2*length]))/float(length)
+
+    return dkt
+
+
+def transpose_blocks(txt, keyLen):
+    lst = []
+    for i in range(keyLen):
+        lst.append('')
+    for i in len(txt):
+        lst[i%keyLen] = txt[i]
+    return lst
+
 
 #Milestone 1. Make code agree to the hamming distance of 37.
 print hamming_distance("this is a test".encode('hex'),"wokka wokka!!!".encode('hex'))
 
-#Milestone 2.
+#Milestone 2. Guess the key length.
+#Shouldn't be that difficult.
+file = '6.txt'
+f = open(file,'r')
+txt = ''
+for line in f:
+    txt += line.decode('base64').encode('hex')
+
+dkt = key_length(txt)
+
+for k,v in dkt.items():
+    print str(k) +"\t:\t"+ str(v)
