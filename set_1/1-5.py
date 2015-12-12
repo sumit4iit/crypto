ecfc4790ae30e899619ec07280c0813e0d875463
sumit agrawal <sumit4iit@gmail.com>
2015-12-12 16:05:46 +0530

    next one please!

--- set_1/1-5.py
+++ set_1/1-5.py
@@ -1,17 +1,9 @@
-import sys
+string = """Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"""
 
-"""
-Implementing repeating key XOR encryption
-"""
+key = "ICE"
 
-f = open('1-5.txt')
-key = sys.argv[1]
-
-j = 0
-for s in f:
-    res = ''
-    for i in range(len(s)):
-        res += hex(ord(s[i]) ^ ord(key[j%len(key)]))[2:]
-        j+=1
-
-    print res
+enc = ''
+for i in range(len(string)):
+    enc += hex(ord(key[i%len(key)])^ord(string[i]))[2:].zfill(2)
+print enc == """0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"""
